c35dc8d50d69a1dcd097a1e2186fce1d17491f15
Ritesh <ritesh.mvteams@gmail.com>
2024-08-28 12:35:40 +0530

    improvde design

--- src/app/(main)/[slug]/page.tsx
+++ src/app/(main)/[slug]/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 
 import { getAllVerses, getChapterDetail } from "@/http/api";
-import { Verse } from "@/types";
+import { Chapter, Verse } from "@/types";
 import { extractChapterNumber } from "@/utils/helpers";
 
 type Props = {
@@ -13,29 +13,65 @@ type Props = {
 
 export default async function ChapterDetail({ params }: Props) {
   const chapNumber = extractChapterNumber(params.slug);
-  const [chapterDetails, verses] = await Promise.all([
-    getChapterDetail(chapNumber),
-    getAllVerses(chapNumber),
-  ]);
 
   return (
     <main>
-      <section className="container ">
-        <ul className="grid grid-cols-1 gap-5 p-10">
-          {verses.map((verse: Verse) => {
-            return (
-              <li key={verse.id} className="card">
-                <Link
-                  href={`/verses?chapter=${chapNumber}&verse=${verse.verse_number}&max=${chapterDetails.verses_count}`}
-                  className="p-4 block"
-                >
-                  {verse?.slug}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+      <section className="container">
+        <Suspense fallback={"Loading..."}>
+          <ListVerses chapNumber={chapNumber} />
+        </Suspense>
       </section>
     </main>
   );
 }
+
+const ListVerses = async ({ chapNumber }: { chapNumber: number }) => {
+  const [chapterDetails, verses]: [Chapter, Verse[]] = await Promise.all([
+    getChapterDetail(chapNumber),
+    getAllVerses(chapNumber),
+  ]);
+
+  if (!chapterDetails || !verses) {
+    return <div>Error...</div>;
+  }
+
+  return (
+    <>
+      <div className="my-10 flex flex-col gap-5">
+        <h2 className="text-center text-accent text-xl font-semibold">
+          Chapter {chapNumber}
+        </h2>
+        <h1 className="text-center font-bold text-3xl font-lexend">
+          {chapterDetails?.name_translated}
+        </h1>
+
+        <p className="text-left text-lg text-primaryText/80">
+          {chapterDetails?.chapter_summary}
+        </p>
+      </div>
+      <ul className="grid grid-cols-1 gap-5 p-10">
+        {verses.map((verse: Verse) => {
+          const translations = verse?.translations?.filter(
+            (t: any) => t.language.toLowerCase() === "english"
+          );
+          return (
+            <li key={verse.id} className="card">
+              <Link
+                href={`/verses?chapter=${chapNumber}&verse=${verse.verse_number}&max=${chapterDetails.verses_count}`}
+                className="p-4 block"
+              >
+                <h4 className="text-accent font-lexend tracking-wider">
+                  Verse {verse?.verse_number}:
+                </h4>
+
+                <p className="mt-2 text-base tracking-wide text-primaryText/80">
+                  {translations[0]?.description}
+                </p>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import { Settings } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
@@ -6,7 +7,12 @@ export default function Header() {
     <header className="bg-secondaryBg text-primaryText py-6">
       <div className="container hover:text-text duration-300">
         <div className="flex justify-between items-center">
-          <h2 className="font-lexend font-bold tracking-widest text-3xl">.</h2>
+          <Link
+            href="/"
+            className="font-lexend font-bold tracking-widest text-3xl"
+          >
+            .
+          </Link>
           <div>
             <button className="p-2 rounded-md hover:bg-primaryBg duration-300 group active:scale-95">
               <Settings className="group-hover:rotate-90 duration-300" />
