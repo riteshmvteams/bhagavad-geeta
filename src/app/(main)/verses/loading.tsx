83c3400e161a0f925964b7fafa9ac2e27ade209d
Ritesh <ritesh.mvteams@gmail.com>
2024-10-24 12:47:15 +0530

    design detail verse page

---
+++ src/app/(main)/verses/loading.tsx
@@ -0,0 +1,11 @@
+import Skelton from "@/components/ui/skelton";
+
+export default function loading() {
+  return (
+    <main className="p-10">
+      <section className="container">
+        <Skelton className="h-8 w-full" />
+      </section>
+    </main>
+  );
+}
--- src/app/(main)/verses/page.tsx
+++ src/app/(main)/verses/page.tsx
@@ -24,11 +24,69 @@ export default async function VersePage({ searchParams }: Props) {
   return (
     <main className="p-10">
       <section className="container">
-        <div>{verse?.text}</div>
+        <h2 className="font-lexend text-xl font-medium text-accent text-center mb-8">
+          Chapter No: {verse.chapter_number} - Verse No: {verse.verse_number}
+        </h2>
+        <div className="flex flex-col gap-3 items-center mb-10">
+          <div className="text-lg font-lexend">{verse?.text}</div>
+          <div className="text-lg font-lexend">{verse?.transliteration}</div>
+        </div>
 
-        <div className="flex items-center gap-2">
+        <ul className="flex flex-col gap-4 pb-4">
+          <h2 className="font-bold text-3xl font-lexend -mb-3">Translation:</h2>
+          {verse?.translations?.map((trans: any, ind: number) => {
+            return (
+              <li
+                key={trans.id}
+                className="card !p-2.5 hover:ring-1 hover:ring-accent flex flex-col gap-5 cursor-pointer"
+              >
+                <div>
+                  <span className="font-lexend text-base text-accent font-bold">
+                    {ind + 1}.
+                  </span>{" "}
+                  {trans?.description}
+                </div>
+                <div className="flex justify-end text-accent font-lexend text-sm">
+                  {trans?.author_name}
+                  {"  "}|
+                  <span className="ml-1.5 capitalize">{trans?.language}</span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+
+        <ul className="flex flex-col gap-4 border-t border-t-accent pt-4">
+          <h2 className="font-bold text-3xl font-lexend -mb-3">
+            Commentories:
+          </h2>
+          {verse?.commentaries?.map((comments: any, ind: number) => {
+            return (
+              <li
+                key={comments.id}
+                className="card !p-2.5 hover:ring-1 hover:ring-accent flex flex-col gap-5 cursor-pointer"
+              >
+                <div>
+                  <span className="font-lexend text-base text-accent font-bold">
+                    {ind + 1}.
+                  </span>{" "}
+                  {comments?.description}
+                </div>
+                <div className="flex justify-end text-accent font-lexend text-sm">
+                  {comments?.author_name}
+                  {"  "}|
+                  <span className="ml-1.5 capitalize">
+                    {comments?.language}
+                  </span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="flex items-center gap-2 justify-center mt-6">
           <Link
-            className={`px-5 py-2 border ${
+            className={`px-6 py-2 border-2 duration-200 rounded-md border-accent hover:bg-accent hover:text-white font-lexend font-semibold active:scale-95 ${
               verse.verse_number === 1 ? "hidden" : ""
             }`}
             href={`/verses?chapter=${searchParams.chapter}&verse=${
@@ -38,7 +96,7 @@ export default async function VersePage({ searchParams }: Props) {
             Prev
           </Link>
           <Link
-            className={`px-5 py-2 border ${
+            className={`px-6 py-2 border-2 duration-200 rounded-md border-accent hover:bg-accent hover:text-white font-lexend font-semibold active:scale-95 ${
               verse.verse_number > Number(searchParams.max) - 1 ? "hidden" : ""
             }`}
             href={`/verses?chapter=${searchParams.chapter}&verse=${
