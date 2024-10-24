ba394cbed2b69e90d219df4fefedcc6cdbd5004d
Ritesh <ritesh.mvteams@gmail.com>
2024-10-24 11:29:55 +0530

    footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
   return (
     <footer className="bg-secondaryBg text-primaryText py-4 mt-auto">
-      <div className="container">Footer</div>
+      <div className="container text-center">
+        <h4 className="text-sm text-accent font-semibold opacity-70">
+          {new Date().getFullYear()}
+        </h4>
+      </div>
     </footer>
   );
 }
